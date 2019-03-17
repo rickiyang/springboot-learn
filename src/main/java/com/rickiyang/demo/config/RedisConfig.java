d1f03d634ec14a3534b484cb1fe8ee541141b64c
rickiyang <medee1320@gmail.com>
2019-03-17 16:13:37 +0800

    提交redis配置

---
+++ src/main/java/com/rickiyang/demo/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.rickiyang.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<String, Integer> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+}
--- src/main/java/com/rickiyang/demo/service/UserService.java
+++ src/main/java/com/rickiyang/demo/service/UserService.java
@@ -3,6 +3,8 @@ package com.rickiyang.demo.service;
 import com.rickiyang.demo.dao.UserDao;
 import com.rickiyang.demo.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,6 +18,8 @@ public class UserService {
 
     @Autowired
     private UserDao userDao;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     public void insert(User user){
         userDao.insert(user);
@@ -23,6 +27,14 @@ public class UserService {
 
     public User queryUserById(long id){
         return userDao.selectByPrimaryKey(id);
+    }
+
+    public void setToRedis(User user){
+        if(user == null) {
+            return;
+        }
+        HashOperations hashOperations = redisTemplate.opsForHash();
+        hashOperations.put("users",user.getId(),user);
     }
 
 
