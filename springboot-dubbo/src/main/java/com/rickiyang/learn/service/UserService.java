b07e982f2b2e90e769453d94315921cbee3ca262
yangyue2 <yangyue2@yy.com>
2019-11-19 00:09:25 +0800

    springboot整合dubbo

---
+++ springboot-dubbo/src/main/java/com/rickiyang/learn/service/UserService.java
@@ -0,0 +1,9 @@
+package com.rickiyang.learn.service;
+
+/**
+ * @author: rickiyang
+ * @date: 2019/11/18
+ * @description:
+ */
+public interface UserService {
+}
---
+++ springboot-dubbo/src/main/java/com/rickiyang/learn/service/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.rickiyang.learn.service.impl;
+
+import com.rickiyang.learn.service.UserService;
+
+/**
+ * @author: rickiyang
+ * @date: 2019/11/18
+ * @description:
+ */
+public class UserServiceImpl implements UserService {
+}
